59a060994bc31aeec53fcd64ab96234172e4c455
J.P. Zambrano <juan.p.zambrano@raytheon.com>
2021-08-02 01:55:57 -0500

    Adds shim for materialize-css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const webpack = require('webpack')
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['i.imgur.com'],
   },
+  webpack: (config, options) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        M: 'materialize-css'
+      })
+    )
+
+    return config
+  }
 }
