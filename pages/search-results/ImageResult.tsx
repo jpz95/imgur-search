0adce988b64ed78a3ad785dc5ef2573a338dbb8d
J.P. Zambrano <juan.p.zambrano@raytheon.com>
2021-08-02 01:49:57 -0500

    Creates image result component

---
+++ pages/search-results/ImageResult.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import { Icon } from 'react-materialize'
+
+import styles from './ImageResult.module.scss'
+
+const ImageResult = (props) => {
+  return (
+    <div className={styles.imageResult}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={props.url}
+          key={props.likes}
+          alt="hi"
+          layout="fill"
+        ></Image>
+      </div>
+      <div className={styles.description}>
+        <div className={styles.title}>{props.title}</div>
+        <span className={styles.likes}>
+          <Icon className={styles.likesIcon}>favorite_border</Icon>
+          <span>{props.likes}</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default ImageResult;
--- pages/search-results/[searchId].tsx
+++ pages/search-results/[searchId].tsx
@@ -1,27 +1,72 @@
+import { useRef, useState } from 'react'
+import { Modal, Button } from 'react-materialize'
+
 import styles from '../../styles/pages/SearchResults.module.scss'
 
 import SearchBar from '../../components/SearchBar/SearchBar'
+import ImageResult from './ImageResult'
 
 const SearchResult = (props: Props) => {
-  const results = [
-    { url: 'some-url1', likes: 1 },
-    { url: 'some-url2', likes: 2 },
-    { url: 'some-url3', likes: 3 },
-    { url: 'some-url4', likes: 4 },
-    { url: 'some-url5', likes: 5 },
-    { url: 'some-url6', likes: 6 },
-  ]
+  const root = useRef(null)
+
+  const [results, setResults] = useState([])
+  setTimeout(() => {
+    setResults([
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 1, id: 1, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 2, id: 2, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 3, id: 3, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 4, id: 4, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 5, id: 5, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 6, id: 6, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 7, id: 7, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 8, id: 8, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 9, id: 9, title: 'kitty' },
+      { url: 'https://i.imgur.com/Wp90pG0.jpg', likes: 10, id: 10, title: 'kitty' },
+    ])
+  }, 3000)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openImageInModal = (imageResult) => {
+    setIsModalOpen(true)
+  }
 
   return (
-    <div className={styles.searchResult}>
+    <div className={styles.searchResult} ref={root}>
       <header className={styles.searchResultHeader}>
         <SearchBar></SearchBar>
       </header>
       <main className={styles.searchResults}>
         {results.map((result) => (
-          <span className={styles.items} key={result.likes}>{result.url}</span>
+          <ImageResult
+            url={result.url}
+            likes={result.likes}
+            title={result.title}
+            key={result.id}
+            onClick={() => openImageInModal(result)}
+          ></ImageResult>
         ))}
       </main>
+      {/* {typeof window !== 'undefined' && root?.current && <Modal
+        actions={[
+          // eslint-disable-next-line react/jsx-key
+          <Button flat modal="close" node="button" waves="green">Close</Button>
+        ]}
+        bottomSheet={false}
+        fixedFooter={false}
+        header="Modal Header"
+        open={isModalOpen}
+        options={{
+          dismissible: true,
+          endingTop: '10%',
+          inDuration: 250,
+          opacity: 0.5,
+          outDuration: 250,
+          preventScrolling: true,
+          startingTop: '4%'
+        }}
+        root={root.current}
+      ></Modal>} */}
     </div>
   )
 }
