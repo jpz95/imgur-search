977088b8ad1062d31cbaf8caf44a306fc5199b5f
J.P. Zambrano <juan.p.zambrano@raytheon.com>
2021-08-01 21:22:43 -0500

    Creates pages style folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/pages/Home.module.scss'
 
 import SearchBar from '../components/SearchBar/SearchBar'
 
